ce1b709e69a2ef01d620c48cb2cdfa3523db4431
Lyrathorne <gashishshishovich@gmail.com>
2026-03-28 16:16:11 +0300

    Create chat.py

---
+++ Lyra/models/chat.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+chat_members = Table(
+    "chat_members",
+    Base.metadata,
+    Column("chat_id", Integer, ForeignKey("chats.id"), primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+)
+
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    is_group = Column(Boolean, default=False)
+
+    members = relationship("User", secondary=chat_members, back_populates="chats")
