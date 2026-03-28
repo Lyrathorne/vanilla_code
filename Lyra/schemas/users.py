f43d4c545ede4b165ba460a8ab81d054f632193f
Lyrathorne <gashishshishovich@gmail.com>
2026-03-28 20:25:42 +0300

    Update users.py

--- Lyra/schemas/users.py
+++ Lyra/schemas/users.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
 
 
-class User(BaseModel):
+class UserCreate(BaseModel):
+    username: str = Field(min_length=3)
+    email: str
+    password: str = Field(min_length=6)
+    is_active: bool = True
+
+
+class UserUpdate(BaseModel):
     username: str = Field(min_length=3)
     email: str
     is_active: bool = True
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_active: bool
+
+    class Config:
+        from_attributes = True
