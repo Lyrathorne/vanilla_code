73609498784d164f0c15deeaa9e2d5197e511236
Lyrathorne <gashishshishovich@gmail.com>
2026-03-28 16:16:21 +0300

    Update users.py

--- Lyra/routers/users.py
+++ Lyra/routers/users.py
@@ -1,38 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 import services.users as service
 from schemas.users import User
+from database import get_db
+
 router = APIRouter()
+
+
 @router.get("/users/search")
-def search_users(username: str):
-          return service.search_user(username)
+def search_users(username: str, db: Session = Depends(get_db)):
+    return service.search_users(db, username)
+
+
 @router.get("/users/active")
-def get_active():
-        return service.search_active()
+def get_active(db: Session = Depends(get_db)):
+    return service.search_active(db)
+
+
 @router.get("/users")
-def get_all_users():
-          return service.get_users()
+def get_all_users(db: Session = Depends(get_db)):
+    return service.get_users(db)
+
+
 @router.get("/users/{user_id}")
-def get_user_by_id(user_id: int):
-          user = service.get_user(user_id)
-          if user is None:
-                    return {"Error": "User not found"}
-          return user
+def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user = service.get_user(db, user_id)
+    if user is None:
+        return {"Error": "User not found"}
+    return user
+
+
 @router.post("/users")
-def create_user(user: User):
-        return service.create_user(user.model_dump())
+def create_user(user: User, db: Session = Depends(get_db)):
+    return service.create_user(db, user.dict())
+
+
 @router.put("/users/{user_id}")
-def redact_user(user_id : int, new_user : User):
-        result = service.redact_user(user_id, new_user.model_dump())
-        if result == None:
-                return{"Error" : "this user doesn`t exist"}
-        return result
+def redact_user(user_id: int, new_user: User, db: Session = Depends(get_db)):
+    result = service.redact_user(db, user_id, new_user.dict())
+    if result is None:
+        return {"Error": "this user doesn`t exist"}
+    return result
+
+
 @router.delete("/users/{user_id}")
-
-def delete_user(user_id: int):
-          result = service.delete_user(user_id)
-          if result is None:
-                  return{"Error" : "this user doesn`t exist"}
-          return result
-        
-
-          
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    result = service.delete_user(db, user_id)
+    if result is None:
+        return {"Error": "this user doesn`t exist"}
+    return result
