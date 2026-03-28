be2cdcfeca23d0dbdc39d1a4f227b30b9313dc18
Lyrathorne <gashishshishovich@gmail.com>
2026-03-28 16:34:12 +0300

    Update main.py

--- Lyra/main.py
+++ Lyra/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from routers import users, chats, messages
 from database import engine, Base
-from Lyra.models import user
+from models import user
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-app.include_router(user.router)
+app.include_router(users.router)
 app.include_router(chats.router)
 app.include_router(messages.router)
