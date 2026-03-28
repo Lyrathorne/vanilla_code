bee13f1637f0f086b6b5366b9eaa4a04d03c04a9
Lyrathorne <gashishshishovich@gmail.com>
2026-03-28 16:16:20 +0300

    Update messages.py

--- Lyra/routers/messages.py
+++ Lyra/routers/messages.py
@@ -1,38 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from schemas.messages import Message
 import services.messages as service
+from database import get_db
 
 router = APIRouter()
 
+
 @router.get("/messages")
-def get_all_messages():
-    return service.get_messages()
+def get_all_messages(db: Session = Depends(get_db)):
+    return service.get_messages(db)
+
 
 @router.get("/messages/{message_id}")
-def get_message_by_id(message_id: int):
-    message = service.get_message(message_id)
+def get_message_by_id(message_id: int, db: Session = Depends(get_db)):
+    message = service.get_message(db, message_id)
     if message is None:
         return {"Error": "Message not found"}
     return message
 
+
 @router.post("/messages")
-def create_message(message: Message):
-    return service.create_message(message.dict())
+def create_message(message: Message, db: Session = Depends(get_db)):
+    return service.create_message(db, message.dict())
+
 
 @router.delete("/messages/{message_id}")
-def delete_message(message_id: int):
-    result = service.delete_message(message_id)
+def delete_message(message_id: int, db: Session = Depends(get_db)):
+    result = service.delete_message(db, message_id)
     if result is None:
         return {"Error": "Message not found"}
     return result
 
+
 @router.get("/chats/{chat_id}/messages")
-def get_messages_by_chat(chat_id: int):
-    return service.get_messages_by_chat(chat_id)
+def get_messages_by_chat(chat_id: int, db: Session = Depends(get_db)):
+    return service.get_messages_by_chat(db, chat_id)
+
 
 @router.patch("/messages/{message_id}/read")
-def mark_message_as_read(message_id: int):
-    result = service.mark_as_read(message_id)
+def mark_message_as_read(message_id: int, db: Session = Depends(get_db)):
+    result = service.mark_as_read(db, message_id)
     if result is None:
         return {"Error": "Message not found"}
     return result
