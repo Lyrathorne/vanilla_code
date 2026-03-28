70959fe1ad7df00c1385a9d173337426c069005d
Lyrathorne <gashishshishovich@gmail.com>
2026-03-28 20:25:38 +0300

    Update chats.py

--- Lyra/schemas/chats.py
+++ Lyra/schemas/chats.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel, Field
 
-class Chat(BaseModel):
+
+class ChatCreate(BaseModel):
     title: str = Field(min_length=1, max_length=50)
+    is_group: bool
+    member_ids: list[int]
+
+
+class ChatOut(BaseModel):
+    id: int
+    title: str
     is_group: bool
     member_ids: list[int]
