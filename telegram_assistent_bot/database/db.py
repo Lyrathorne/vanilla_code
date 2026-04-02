981af2998223b1f9cea0ff7fe2043d35fafcd798
Lyrathorne <gashishshishovich@gmail.com>
2026-04-02 15:50:47 +0300

    Update db.py

--- telegram_assistent_bot/database/db.py
+++ telegram_assistent_bot/database/db.py
@@ -1,11 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
+
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./db.sqlite3"
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
-Session = async_sessionmaker(
+SessionLocal = async_sessionmaker(
     
     autoflush=False,
     bind=engine,
@@ -13,3 +14,6 @@ Session = async_sessionmaker(
 )
 
 Base = declarative_base()
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
