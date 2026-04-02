dce0fea642f67779503c23dcf8eb23f3cfb762fb
Lyrathorne <gashishshishovich@gmail.com>
2026-04-02 15:50:49 +0300

    Update models.py

--- telegram_assistent_bot/database/models.py
+++ telegram_assistent_bot/database/models.py
@@ -1,11 +1,15 @@
 from db import Base
 from sqlalchemy import Column, Integer, String,  DateTime
-from datetime import datetime
+from datetime import datetime, timezone
+def get_time():
+        current_time = datetime.now(timezone.utc)
+        return(current_time)
+        
 
 class User(Base):
           __tablename__ = "users"
           id = Column("id", Integer, primary_key=True)
-          telegram_id = Column("telegram_id", Integer)
+          telegram_id = Column("telegram_id", Integer, unique=True)
           username = Column("username", String, nullable=True)
           first_name = Column("first_name", String)
-          created_at = Column("created_at", DateTime)     
+          created_at = Column("created_at", DateTime, default= get_time)     
