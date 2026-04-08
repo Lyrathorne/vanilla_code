b0203c593389a93ccd0d05bcf9a75f6726df0e19
Lyrathorne <gashishshishovich@gmail.com>
2026-04-08 12:28:58 +0300

    1
    1

--- telegram_assistent_bot/database/models.py
+++ telegram_assistent_bot/database/models.py
@@ -35,6 +35,8 @@ class User(Base):
     username = Column("username", String, nullable=True)
     first_name = Column("first_name", String)
     created_at = Column("created_at", DateTime, default=get_time)
+    language = Column("language", String, default="eng" )
+    timzone = Column("timezone", String, default="UTC")
 
 
 class Task(Base):
@@ -46,7 +48,21 @@ class Task(Base):
     status = Column("status", Boolean, default=False)
     created_at = Column("created_at", DateTime, default=get_time)
     deadline = Column("deadline", DateTime, nullable=True)
+    comleted_at = Column("completed_at", DateTime, nullable=True)
 
     reminded_day = Column("reminded_day", Boolean, default=False)
     reminded_hour = Column("reminded_hour", Boolean, default=False)
     reminded_overdue = Column("reminded_overdue", Boolean, default=False)
+
+class Scheduleitem(Base):
+    __tablename__ = "schedule"
+
+    id = Column("id", Integer, primary_key=True)
+    user_id = Column("user_id", Integer, unique=True)
+    title = Column("title", String)
+    date = Column("date", String)
+    start_time = Column("start_time", DateTime)
+    end_time = Column("end_time", DateTime)
+    location = Column("location", String)
+    comment = Column("comment", String)
+    created_at = Column("created_at", DateTime)
